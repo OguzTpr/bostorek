1e43cb794d5aa7951a4382694b8a359f5419764f
Oguz Tpr <oguztp86@gmail.com>
2024-05-27 07:52:39 +0300

    created local book database, books components

---
+++ frontend/src/db.js
@@ -0,0 +1,104 @@
+const books = [
+    {
+        id: 1,
+        name: 'Rüzgarın Şarkısını Dinle',
+        author: 'Haruki Murakami',
+        page: 168,
+        description: 'Herkes yürekten verdiğinin karşılığını alır. Kesinlikle güzel biri değildi. Ancak “güzel biri değildi” demekle ona haksızlık etmiş olurum.',
+        rating: 8.5,
+        image: '5959390',
+        uploadDate: '21 Aug 2023'
+    },
+    {
+        id: 2,
+        name: 'Ortadan Kaybolan Fil',
+        author: 'Haruki Murakami',
+        page: 360,
+        description: '“Sana yardım etmeme izin verirsen bir şeyler değişebilir” diye fısıldadı cüce. “Nasıl bir yardım bu?” diye merakla sordum hemen.',
+        rating: 7.5,
+        image: '11803611',
+        uploadDate: '07 May 2024'
+    },
+    {
+        id: 3,
+        name: 'Kadınsız Erkekler',
+        author: 'Haruki Murakami',
+        page: 224,
+        description: 'Bir gün sen de kadınsız erkeklerden olacaksın. O gün en ufak bir uyarı, küçücük bir ipucu vermeden; önsezi olarak hissettirmeden ya da içine doğmadan; kapını çalmadan, hiç beklemediğin bir anda seni bulacak.',
+        rating: 6.5,
+        image: '1133181',
+        uploadDate: '15 Mar 2022'
+    },
+    {
+        id: 4,
+        name: 'Pinball 1973',
+        author: 'Haruki Murakami',
+        page: 144,
+        description: 'Bir pinball makinesinden hiçbir şey kazanamayız. Sayıya dönüştürülmüş gurur dışında. Öte yandan kaybedeceklerimiz gerçekten de çok fazladır.',
+        rating: 5.5,
+        image: '11310719',
+        uploadDate: '21 Nov 2020'
+    },
+    {
+        id: 5,
+        name: 'Fare Dörtlemesi',
+        author: 'Haruki Murakami',
+        page: 928,
+        description: 'Başlarda “Fare Üçlemesi” olarak anılan seri Rüzgârın Şarkısını Dinle, Pinball 1973 ve Yaban Koyununun İzinde’den oluşur.',
+        rating: 4.5,
+        image: '11826628',
+        uploadDate: '12 Jan 2024'
+    },
+    {
+        id: 6,
+        name: '1Q84 1. Kitap',
+        author: 'Haruki Murakami',
+        page: 470,
+        description: 'Sarsıcı bir yolculuğa hazır mısınız? Öyleyse kemerlerinizi bağlayın. Erkekleri, titizlikle geliştirdiği bir yöntemle öteki dünyaya gönderen genç bir kadınla tanışacaksınız.',
+        rating: 5.5,
+        image: '1108278',
+        uploadDate: '28 Oct 2016'
+    },
+    {
+        id: 7,
+        name: 'İmkansızın Şarkısı',
+        author: 'Haruki Murakami',
+        page: 349,
+        description: 'Bir yolculuk sırasında Beatles’ın “Norwegian Wood” adlı parçasını duyan kahramanımız 37 yaşındadır ve bu parça onu Tokyo’da geçirdiği üniversite yıllarına götürecektir.',
+        rating: 7.5,
+        image: '1113480',
+        uploadDate: '14 Dec 2015'
+    },
+    {
+        id: 8,
+        name: 'Kadınsız Erkekler',
+        author: 'Haruki Murakami',
+        page: 224,
+        description: 'Bir gün sen de kadınsız erkeklerden olacaksın. O gün en ufak bir uyarı, küçücük bir ipucu vermeden; önsezi olarak hissettirmeden ya da içine doğmadan; kapını çalmadan, hiç beklemediğin bir anda seni bulacak.',
+        rating: 6.5,
+        image: '1133181',
+        uploadDate: '15 Mar 2022'
+    },
+    {
+        id: 9,
+        name: 'Karanlıktan Sonra',
+        author: 'Haruki Murakami',
+        page: 180,
+        description: 'Gece yarısından sonra zamanın kendine özgü bir akışı vardır. Ona karşı koyamazsın…',
+        rating: 7.5,
+        image: '3813748',
+        uploadDate: '25 May 2017'
+    },
+    {
+        id: 10,
+        name: 'Zemberek Kuşu nun Güncesi',
+        author: 'Haruki Murakami',
+        page: 738,
+        description: 'Japon edebiyatının aykırı çocuğu Murakami Haruki "Zemberek Kuşu’nun Güncesi" adlı, kendisine dünya edebiyatında önemli bir yer açan romanıyla Türkçe.',
+        rating: 4.5,
+        image: '271176',
+        uploadDate: '05 Nov 2015'
+    }
+]
+
+export default books;
--- frontend/src/view/books.vue
+++ frontend/src/view/books.vue
@@ -2,39 +2,24 @@
 <section>
     <div class="container">
 <sectionHeader title="B.o.o.k.s" text="Some text about books"/>
-<div class="row">
-<div class="col-sm-6 col-md-4 col-xl-3">
-    <div class="card border-0 shadow position-relative">
-        <img src="https://img.kitapyurdu.com/v1/getImage/fn:11803611/" class="card-img-top" />
-        <div class="card-body">
-            <div class="auth-box">
-                <span style="background-color: cadetblue;" class="py-1 px-3 text-white rounded-pill">
-                Haruki Murakami
-                </span>
-            </div>
-            <h5 class="card-title mt-3 fw-semibold">Ortadan Kaybolan Fil</h5>
-            <p class="card-text">Haruki Murakami’nin 1980-1991 yılları arasında yazdığı, 17 öyküden oluşan Ortadan Kaybolan Fil, kitap olarak önce İngilizce yayımlandı. Kitap haline gelmeden önce öykülerin bazıları The New Yorker, Playboy gibi dergilerde yer aldı. Ortadan Kaybolan Fil Haruki Murakami edebiyatının kaynaklarına dair çok önemli ipuçları veren, yazarın 1Q84, Zemberekkuşu’nun Güncesi gibi kült eserlerinin çıkış noktası olan öykülerin yanı sıra Şüphe adıyla filme uyarlanan “Ambar Yakmak”ın yer aldığı, Murakami hayranlarının uzun zamandır beklediği bir kitap.</p>
-            <div class="d-flex justify-content-between align-items-center">
-                <a href="#" class="card-link">Read more</a>
-                <p style="background-color: cadetblue" class="py=1 px=3 text-white badge">19 jan 2022</p>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
+<BookList :books="books"/>
 </div>
 </section>
 </template>
 
 <script>
+import BookList from '@/components/booklist.vue';
 import sectionHeader from '@/components/sectionheader.vue';
+import books from '@/db.js';
     export default {
         name: "books",
         components:{
-            sectionHeader
+            sectionHeader,
+            BookList
         },
         data() {
             return{
+                books: books
             }
         }
         
